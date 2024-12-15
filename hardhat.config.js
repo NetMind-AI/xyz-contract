2a385186853341a67293d46f7cab19c93fe7dc59
routery <rout@protagonist-us.com>
2024-12-15 16:08:48 +0800

    update Bonding

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,41 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require('dotenv').config({ path: __dirname + '/.env' })
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
-};
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.22",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        },
+      },
+    ],
+  },
+  networks: {
+    bsc: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    bscTestnet: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+  },
+  sourcify: {
+    enabled: true,
+  },
+  etherscan: {
+    apiKey: {
+      bsc: process.env.ABI_KEY,
+      bscTestnet: process.env.ABI_KEY,
+    },
+  }
+}
